53b17cadb72c0c6d22f2b871dbfbb05dc4917e37
Alucard24 <dmbaoh2@gmail.com>
2015-09-02 12:33:08 +0200

    FS: Updated sdcardfs from Official LP HOF2 sourcecode

--- include/linux/magic.h
+++ include/linux/magic.h
@@ -8,7 +8,7 @@
 #define CODA_SUPER_MAGIC	0x73757245
 #define CRAMFS_MAGIC		0x28cd3d45	/* some random number */
 #define CRAMFS_MAGIC_WEND	0x453dcd28	/* magic number with the wrong endianess */
-#define DEBUGFS_MAGIC          0x64626720
+#define DEBUGFS_MAGIC		0x64626720
 #define SECURITYFS_MAGIC	0x73636673
 #define SELINUX_MAGIC		0xf97cff8c
 #define RAMFS_MAGIC		0x858458f6	/* some random number */
@@ -69,5 +69,6 @@
 #define MTD_INODE_FS_MAGIC      0x11307854
 #define ANON_INODE_FS_MAGIC	0x09041934
 
+#define SDCARDFS_SUPER_MAGIC    0xb550ca10
 
 #endif /* __LINUX_MAGIC_H__ */
